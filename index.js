8ed0929a0be451cdd009ef85947707d21c6c6c09
heliobrandao <heliob79@gmail.com>
2022-02-17 21:19:01 -0300

    2nd-class

--- index.js
+++ index.js
@@ -6,11 +6,38 @@ const fs = require('fs);
 import chalk from 'chalk';
 import * as fs from 'fs';
 
-function pegaArquivo(caminhoDoArquivo){
-  const encoding = 'utf-8'
-  fs.readFile(caminhoDoArquivo, encoding,(_, texto)=>{
-    console.log(chalk.green(texto));
-  })
+function trataErro(erro){
+  throw new Error(chalk.red(erro.code, 'não há arquivo'));
 }
+
+
+async function pegaArquivo(caminhoDoArquivo){
+  const encoding = 'utf-8';
+  try{
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto))
+  } catch(erro) {
+    trataErro(erro);
+  }
+
+}
+
+// function pegaArquivo(caminhoDoArquivo){
+//   const encoding = 'utf-8';
+//   fs.promises
+//   .readFile(caminhoDoArquivo, encoding)
+//   .then((texto) => console.log(texto))
+//   .catch((erro) => trataErro(erro))
+// }
+
+// function pegaArquivo(caminhoDoArquivo){
+//   const encoding = 'utf-8'
+//   fs.readFile(caminhoDoArquivo, encoding,(erro, texto)=>{
+//     if (erro){
+//       trataErro(erro);
+//     }
+//     console.log(chalk.green(texto));
+//   })
+// }
 
 pegaArquivo('./arquivos/texto1.md');
