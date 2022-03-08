46a83ad67398b8ed9e090f6ae5f3a87d12dd9d9a
heliobrandao <heliob79@gmail.com>
2022-03-07 23:10:42 -0300

    5th-class

--- cli.js
+++ cli.js
@@ -1,12 +1,17 @@
 const chalk = require('chalk');
 const pegaArquivo = require('./index.js');
+const validaURLs = require('./http-validacao')
 //import pegaArquivo from ('./index');
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDeArquivo){
   const resultado = await pegaArquivo(caminho[2]);
-  console.log(chalk.yellow('lista de links'), resultado);
+  if (caminho[3] === 'validar') {
+    console.log(chalk.yellow('links validados'), await validaURLs(resultado));
+  } else {
+    console.log(chalk.yellow('lista de links'), resultado);
+  }
 }
 
 processaTexto(caminho);
